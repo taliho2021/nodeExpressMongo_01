a65ca8b48d97cfd7f7e7c74e3b80422311656d0e
taliho2021 <tyyo1980@gmail.com>
2021-07-13 16:27:39 -0400

    Updated user model to include bcryptjs

--- models/user.js
+++ models/user.js
@@ -9,14 +9,48 @@ const userSchema = new mongoose.Schema({
     date: Date,
 })
 
-//hash the password
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
-}
+// //hash the password
+// userSchema.methods.generateHash = function(password) {
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+// }
 
-// checking is password is valild
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.passowrd)
-}
+// // checking is password is valild
+// userSchema.methods.validPassword = function(password) {
+//     return bcrypt.compareSync(password, this.passowrd)
+// }
+
+userSchema.pre("save", function (next) {
+    const user = this
+  
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function(hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+  
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
+  })
+  
+  userSchema.methods.comparePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(error, isMatch) {
+      if (error) {
+        return callback(error)
+      } else {
+        callback(null, isMatch)
+      }
+    })
+  }
+
 
 module.exports= mongoose.model('User', userSchema)
